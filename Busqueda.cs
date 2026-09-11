4b895975e0b76709eb5cb0c98c3562ab1662d6c1
25030252nes <25030252@itesa.edu.mx>
2026-09-11 00:23:48 -0600

    Agregar archivos de proyecto.

---
+++ Busqueda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Busqueda_binario_con_analisis_de_complejidad
+{
+    public class Busqueda
+    {
+        public static int Binaria(int[] a, int x)
+        {
+            int i = 0, f = a.Length - 1;
+            while (i <= f)
+            {
+                int m = (i + f) / 2;
+                if (a[m] == x) return m;
+                if (a[m] < x) i = m + 1;
+                else f = m - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Busqueda_binario_con_analisis_de_complejidad;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] datos = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Posición: " + Busqueda.Binaria(datos, 30));
+        Console.WriteLine("Tiempo: O(log n)");
+        Console.WriteLine("Espacio: O(1)");
+    }
+}
